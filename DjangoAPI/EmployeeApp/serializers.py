a0277b452533098f463541a2cb66902032841ec2
Ameyht <153919788+Ameyht@users.noreply.github.com>
2024-10-03 11:47:51 +0530

    Add files via upload

--- DjangoAPI/EmployeeApp/serializers.py
+++ DjangoAPI/EmployeeApp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from EmployeeApp.models import Departments,Employees, Users
+from EmployeeApp.models import Departments,Employees, Users, Attendance, LeaveRequest,Projects
 
 class DepartmentSerializers(serializers.ModelSerializer):
     class Meta:
@@ -9,9 +9,24 @@ class DepartmentSerializers(serializers.ModelSerializer):
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model=Employees
-        fields=('EmployeeId','EmployeeName','Department','DateOfJoining','PhotoFileName')
+        fields=('EmployeeId','EmployeeName','Department','DateOfJoining','PhotoFileName','Role','Manager','ProjectId')
 
 class UserSerializers(serializers.ModelSerializer):
     class Meta:
         model=Users
         fields=('UserId','username','password','role','firstName','lastName','emailId','phoneNumber','status')
+
+class LeaveRequestSerializers(serializers.ModelSerializer):
+    class Meta:
+        model=LeaveRequest
+        fields=('LeaveId','EmployeeName','LeaveDate','Reason','Status','ReportingManager')
+
+class AttendanceSerializers(serializers.ModelSerializer):
+    class Meta:
+        model=Attendance
+        fields=('AttendanceId','Status')
+
+class ProjectSerializers(serializers.ModelSerializer):
+    class Meta:
+        model=Projects
+        fields=('ProjectId','ProjectName','ReportingManager','Origin','StartedDate','Requirements','WorkingEmployeeIds')
--- DjangoAPI/EmployeeApp/views.py
+++ DjangoAPI/EmployeeApp/views.py
@@ -5,8 +5,8 @@ from django.http.response import JsonResponse
 from rest_framework.decorators import api_view,permission_classes
 from .utils import generate_jwt_token,check_jwt_token
 
-from EmployeeApp.models import Departments,Employees,Users
-from EmployeeApp.serializers import DepartmentSerializers,EmployeeSerializer,UserSerializers
+from EmployeeApp.models import Departments,Employees,Users, Attendance,LeaveRequest ,Projects
+from EmployeeApp.serializers import DepartmentSerializers,EmployeeSerializer,UserSerializers,AttendanceSerializers,LeaveRequestSerializers,ProjectSerializers
 
 # Create your views here.
 @csrf_exempt
@@ -43,17 +43,22 @@ def departmentApi(request,id=0):
     
 @check_jwt_token
 @csrf_exempt
-def employeeApi(request,id=0):
+def employeeApi(request,id=0,username="undefined"):
     if request.method=='GET':
         try:
             if id!=0:
                 employee=Employees.objects.get(EmployeeId=id)
                 employees_serializer=EmployeeSerializer(employee,many=False)
                 return JsonResponse(employees_serializer.data,safe=False)
-            else:
+            elif username!='undefined':
+                employee=Employees.objects.filter(Manager=username)
+                employees_serializer=EmployeeSerializer(employee,many=True)
+                return JsonResponse(employees_serializer.data,safe=False)
+            else :
                 employees=Employees.objects.all()
                 employees_serializer=EmployeeSerializer(employees,many=True)
                 return JsonResponse(employees_serializer.data,safe=False)
+            
         except Employees.DoesNotExist:
             return JsonResponse({"message":"User Not found!"},safe=False)
         except Exception as e:
@@ -66,14 +71,26 @@ def employeeApi(request,id=0):
             return JsonResponse("Employee Added Successfully",safe=False)
         return JsonResponse("Failed to Add Employee",safe=False)
     elif request.method == 'PUT':
-        try:    
-            employee=JSONParser().parse(request)
-            db_emp=Employees.objects.get(EmployeeId=employee["EmployeeId"])
-            employees_serializer=EmployeeSerializer(db_emp,data=employee)
-            if employees_serializer.is_valid():
-                employees_serializer.save()
-                return JsonResponse("Employee Updated Successfully",safe=False)
-            return JsonResponse("Failed to update the Employee",safe=False)
+        try:
+            employee_id = request.headers.get('EmployeeId')
+            employee=Employees.objects.get(EmployeeId=employee_id)
+            if employee_id == None:
+                employee=JSONParser().parse(request)
+                db_emp=Employees.objects.get(EmployeeId=employee["EmployeeId"])
+                employees_serializer=EmployeeSerializer(db_emp,data=employee)
+                if employees_serializer.is_valid():
+                    employees_serializer.save()
+                    return JsonResponse("Employee Updated Successfully",safe=False)
+                return JsonResponse("Failed to update the Employee",safe=False)
+            else :
+                if employee.Role == 'Manager':
+                    employee=JSONParser().parse(request)
+                    db_emp=Employees.objects.get(EmployeeId=employee["EmployeeId"])
+                    employees_serializer=EmployeeSerializer(db_emp,data=employee)
+                    if employees_serializer.is_valid():
+                        employees_serializer.save()
+                        return JsonResponse("Project Assigned to the Employee",safe=False)
+                    return JsonResponse("Failed to Assign the Employee",safe=False)
         except Employees.DoesNotExist:
             return JsonResponse({"message":"User Not found!"},safe=False)
         except Exception as e:
@@ -143,7 +160,94 @@ def userApi(request,id=0,username="undefined"):
         except Exception as e:
             return JsonResponse({"message":str(e)},safe=False)
 
+@csrf_exempt
+def LeaveApi(request,id=0,username="undefined"):
+    try:
+        employee_id = request.headers.get('EmployeeId')
+        if not employee_id:
+            return JsonResponse("EmployeeId not found in headers", safe=False, status=400)
+        employee=Employees.objects.get(EmployeeId=employee_id)
 
+        if request.method == 'POST':
+            leave=JSONParser().parse(request)
+            leave['EmployeeName']=employee.EmployeeName
+            leave['ReportingManager']=employee.Manager
+            leave_serializer=LeaveRequestSerializers(data=leave)
+            if leave_serializer.is_valid():
+                leave_serializer.save()
+                return JsonResponse("Leave Request created successfully",safe=False)
+            return JsonResponse("Failed to create a leave request",safe=False)
+        elif request.method == 'GET':
+            if employee.Role == 'Manager':
+                leaves=LeaveRequest.objects.filter(ReportingManager=employee.EmployeeName)
+                leave_serializer=LeaveRequestSerializers(leaves,many=True)
+                return JsonResponse(leave_serializer.data,safe=False)
+            else:
+                leaves=LeaveRequest.objects.filter(EmployeeName=employee.EmployeeName)
+                leave_serializer=LeaveRequestSerializers(leaves,many=True)
+                return JsonResponse(leave_serializer.data,safe=False)
+        elif request.method == 'PUT':
+            if employee.Role == 'Manager':
+                leave=JSONParser().parse(request)
+                db_leave=LeaveRequest.objects.get(LeaveId=leave['LeaveId'],ReportingManager=employee.EmployeeName)
+                leave_serializer=LeaveRequestSerializers(db_leave,data=leave)
+                if leave_serializer.is_valid():
+                    leave_serializer.save()
+                    return JsonResponse("Updated the status of leave Request",safe=False)
+            return JsonResponse("You need to be a Manager to access the resource",safe=False)
+    except Employees.DoesNotExist:
+        return JsonResponse({"message":"User Not found!"},safe=False)
+    except Exception as e:
+        return JsonResponse({"message":str(e)},safe=False)  
+
+
+@check_jwt_token
+@csrf_exempt
+def attendanceApi(request,id=0,username="undefined"):
+    if request.method=='GET':
+        print('inside Get')
+        # try:
+        #     if id!=0:
+        #         user=Users.objects.get(UserId=id)
+        #         users_serializer=UserSerializers(user,many=False)
+        #         return JsonResponse(users_serializer.data, safe=False)
+        #     elif username!="undefined":
+        #         user=Users.objects.get(username=username)
+        #         users_serializer=UserSerializers(user,many=False)
+        #         return JsonResponse(users_serializer.data, safe=False)
+        #     else:
+        #         users=Users.objects.all()
+        #         users_serializer=UserSerializers(users,many=True)
+        #         return JsonResponse(users_serializer.data, safe=False)
+        # except Users.DoesNotExist:
+        #     return JsonResponse({"message":"User Not found!"},safe=False)
+        # except Exception as e:
+        #     return JsonResponse({"message":str(e)},safe=False)
+    elif request.method=='POST':
+        attendance=JSONParser().parse(request)
+        attendance_serializer=AttendanceSerializers(data=attendance)
+        if attendance_serializer.is_valid():
+            attendance_serializer.save()
+            return JsonResponse("User Added Successfully",safe=False)
+        return JsonResponse("Failed to Add User",safe=False)
+
+
+@check_jwt_token
+@csrf_exempt
+def projectApi(request,id=0,username="undefined"):
+    try:
+        if request.method == 'POST':
+            project=JSONParser().parse(request)
+            project_serializer=ProjectSerializers(data=project)
+            if project_serializer.is_valid():
+                project_serializer.save()
+                return JsonResponse("Project Registered Successfully",safe=False)
+            return JsonResponse("Failed to register the project",safe=False)
+    except Employees.DoesNotExist:
+        return JsonResponse({"message":"User Not found!"},safe=False)
+    except Exception as e:
+        return JsonResponse({"message":str(e)},safe=False) 
+   
 
 @api_view(['GET'])
 def token(request,username="undefined",password="undefined"):
