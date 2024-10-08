7b34a695b14f6563fc49a53e39c504aeb28e0db5
Ameyht <153919788+Ameyht@users.noreply.github.com>
2024-10-08 14:17:38 +0530

    Add files via upload

---
+++ EmployeeManagementReactJs/employeemanagement/src/components/UserAuth.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Box } from "@mui/material";
+import Login from "./Login";
+import Footer from "./Footer";
+import { useLocation } from "react-router-dom";
+import SignUp from "./SignUp";
+
+import Registered from "./Registered";
+import Header from "./Header";
+
+const UserAuth = () => {
+  const myStyles = {
+    fontFamily: "'Oswald', sans-serif",
+  };
+
+  const location = useLocation();
+  const currentUrl = location.pathname;
+  console.log("CurrentURL:", currentUrl);
+
+  const renderContent = () => {
+    if (currentUrl === "/login") {
+      return <Login />;
+    } else if (currentUrl === "/signup") {
+      return <SignUp />;
+    } 
+   
+     else if (currentUrl === "/registered") {
+      return <Registered />;
+    }
+
+  };
+
+  return (
+    <Box className="md:min-h-16 min-h-16 h-auto md:h-auto" style={myStyles}>
+      <Header />
+      {renderContent()}
+      <Footer />
+    </Box>
+  );
+};
+
+export default UserAuth;
